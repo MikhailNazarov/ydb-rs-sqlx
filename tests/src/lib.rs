7c08ea4fa59d58ed8fa2ad6d1d40f3bc2602f125
Михаил Назаров <mikhail.a.nazarov@yandex.ru>
2024-11-04 11:20:25 +0300

    test_optional_string was added

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,42 +1,55 @@
+
+
 pub mod connect;
 
 #[cfg(test)]
-mod tests{
-    pub use std::env;
-    pub use sqlx::{Acquire, Executor};
-    pub use ydb_sqlx::{with_name, YdbPool, YdbPoolOptions};
-
-    pub struct TestContext{
-        pool: ydb_sqlx::YdbPool
-    }
-    
-    
-    impl TestContext{
-        pub async fn new()->TestContext{
-            let connection_string = env::var("YDB_CONNECTION_STRING").unwrap();
-            let pool = YdbPoolOptions::new()
-                .connect(&connection_string).await.unwrap();
-            TestContext{pool}
-        }
-    
-        pub fn pool(&self)->&YdbPool{
-            &self.pool
-        }
-    
-       
-    }
+pub mod tests{
+    pub use ydb_sqlx::database::Ydb;
+    pub use sqlx::Acquire;
+    pub use ydb_sqlx::with_name;
+    pub use sqlx::pool;
 }
 
+#[tokio::test]
+pub async fn test_optional_string(){
+   
 
+    use tests::*;
+    let pool = Ydb::connect_env().await.unwrap();
 
+    let mut tr = pool.begin().await.unwrap();
+    let conn = tr.acquire().await.unwrap();
+   
+    sqlx::query(r#"
+        CREATE TABLE IF NOT EXISTS test_opt(
+            id Int32 NOT NULL,
+            title Utf8,
+            PRIMARY KEY (id)
+        )
+    "#).execute(conn.schema()).await.unwrap();
+   
+    
+    sqlx::query(r#"
+        insert into test_opt(id, title) values
+        (1, 'title1'),
+        (2, 'title2'),
+        (3, 'title3')
+    "#).execute(&mut *conn).await.unwrap();
 
+    let rows = sqlx::query_as::<_,(i32, Option<String>)>(r#"
+        select * from test_opt
+    "#).fetch_all(&mut *conn).await.unwrap();
+
+    assert_eq!(3, rows.len());
+}
 
 #[tokio::test]
 pub async fn test_opt(){
     use tests::*;
-    let ctx = TestContext::new().await;
+   
+    let pool = Ydb::connect_env().await.unwrap();
 
-    let mut tr = ctx.pool().begin().await.unwrap();
+    let mut tr = pool.begin().await.unwrap();
     let conn = tr.acquire().await.unwrap();
    
     sqlx::query(r#"
